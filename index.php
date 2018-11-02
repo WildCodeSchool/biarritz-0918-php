e3ad0a693eabbcdb9100fcd520fc9ff276b8629e
zazapeta <sami.ghazouane@gmail.com>
2018-11-02 15:17:12 +0100

    Added Model a display of Items

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
 require_once "vendor/autoload.php";
 
 use App\Bag;
+use App\DB;
 use App\Item;
+
+$db = new DB();
+
+$modelItems = $db->select('SELECT * FROM Items', 'App\Model');
+
+// var_dump($db->insert('INSERT INTO Items(name,value,volume) VALUES("gold",100,2)'));
 
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader);
 
-$items = array(
-    new Item([
-        'name' => 'gold',
-        'value' => 3,
-        'volume' => 1,
-    ]),
-);
+$items = array_map(function ($modelItem) {
+    return new Item(get_object_vars($modelItem));
+}, $modelItems);
 
 $bags = array(
     new Bag(10),
